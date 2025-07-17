f5851a861e3f243858ce349c9fa295e72f9eb454
infan11 <infanjiounrahman20606@gmail.com>
2025-07-17 10:59:19 +0600

    Truple

---
+++ truple.ts
@@ -0,0 +1,6 @@
+
+// Tuple হলো এমন একটি অ্যারে যার মধ্যে নির্দিষ্ট সংখ্যক উপাদান থাকবে এবং প্রতিটি উপাদানের টাইপ আগে থেকেই নির্ধারিত থাকবে।
+// যেমন: প্রথমটি string, দ্বিতীয়টি number।
+// ekane  eta hosse wrong let person : [string , number] = [ 21 , "infan" ,]  err dibe 
+// let person : [string , number] = [  "infan" ,21 ,] // eta right way for truple
+let person: [string, number] = ["infan", 21,]
